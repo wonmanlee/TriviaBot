761f84757c3c3c89a512800388b40129c707e112
Wonman Lee <wonmanlee@gmail.com>
2015-03-23 17:41:45 -0400

    Edited home view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,13 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    
+    @categories = Question.pluck(:category_title).uniq.shuffle.first(16)
   end
 
   def leaderboard
     User.find_each do |u|
       u.update(score: u.total_score)
     end
-    @users = User.order(score: :desc).page(params[:page])
+    User.order(score: :desc).page(params[:page])
   end
 end
